d319b28bb23a200781bf328fb1547594e2a3f5f4
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2022-04-16 23:52:25 +0700

    [AC-0] add printf

--- lib/main.dart
+++ lib/main.dart
@@ -51,12 +51,8 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    print("AnnJA");
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
