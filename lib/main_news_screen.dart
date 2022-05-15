911bb6716bd05159de4dbcb771824b5c2a2c9b76
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2022-05-16 00:03:01 +0700

    [AC-4] Extract NewsRecord widget

--- lib/main_news_screen.dart
+++ lib/main_news_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:lottie/lottie.dart';
 import 'package:news_app/news_category.dart';
+import 'package:news_app/news_record.dart';
 import 'package:news_app/news_status.dart';
 
 class MainNewsScreen extends StatelessWidget {
@@ -27,8 +28,8 @@ class MainNewsScreen extends StatelessWidget {
     {
       "title":
           "4jkjkjkjkjkjkjkjkjjkjk444jkjkjkjkjkjkjkjkjjkjk444jkjkjkjkjkjkjkjkjjkjk44",
-      "status": NewsStatus.pendingResponse,
-      "category": NewsCategory.finance
+      // "status": NewsStatus.pendingResponse,
+      // "category": NewsCategory.finance
     },
   ];
   @override
@@ -72,124 +73,44 @@ class MainNewsScreen extends StatelessWidget {
           ],
         ),
         body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
 
-          // child: SingleChildScrollView(
-          //   child: Column(
-          //     crossAxisAlignment: CrossAxisAlignment.stretch,
-          //     children: [
-          //       ...new List<int>.generate(10, (i) => i + 1)
-          //           .map(
-          //             (number) => Container(
-          //               child: Text(
-          //                 "${number}",
-          //                 style: TextStyle(fontSize: 150),
-          //               ),
-          //               color: (number % 2 == 0)
-          //                   ? Colors.amber
-          //                   : Colors.blueAccent,
-          //               height: 100,
-          //               width: 10,
-          //             ),
-          //           )
-          //           .toList()
-          //     ],
-          //   ),
-          // ),
+            // child: SingleChildScrollView(
+            //   child: Column(
+            //     crossAxisAlignment: CrossAxisAlignment.stretch,
+            //     children: [
+            //       ...new List<int>.generate(10, (i) => i + 1)
+            //           .map(
+            //             (number) => Container(
+            //               child: Text(
+            //                 "${number}",
+            //                 style: TextStyle(fontSize: 150),
+            //               ),
+            //               color: (number % 2 == 0)
+            //                   ? Colors.amber
+            //                   : Colors.blueAccent,
+            //               height: 100,
+            //               width: 10,
+            //             ),
+            //           )
+            //           .toList()
+            //     ],
+            //   ),
+            // ),
 
-          child: ListView.builder(
-              itemCount: newsList.length,
-              itemBuilder: (ctx, index) => Container(
-                  padding: EdgeInsets.all(0),
-                  height: 100,
-                  // color: index % 2 == 0 ? Colors.amber : Colors.blue,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        onPrimary: Colors.black,
-                        shadowColor: Colors.transparent,
-                        padding: EdgeInsets.all(0)),
-                    onPressed: () {},
-                    child: Row(
-                      children: [
-                        Image.asset(
-                            (newsList[index]["category"] is NewsCategory)
-                                ? (newsList[index]["category"] as NewsCategory)
-                                    .imagepath
-                                : "assets/images/default.png"),
-                        // Image.network(
-                        //     "https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg"),
-                        Container(
-                          width: 10,
-                        ),
-                        Flexible(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                (newsList[index]["title"] is String)
-                                    ? (newsList[index]["title"])
-                                    : "",
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 1,
-                                softWrap: false,
-                                // style: TextStyle(backgroundColor: Colors.red),
-                              ),
-
-                              SizedBox(
-                                height: 25,
-                                child: IgnorePointer(
-                                  ignoring: true,
-                                  child: ElevatedButton(
-                                      child: Text((newsList[index]["status"]
-                                              is NewsStatus)
-                                          ? (newsList[index]["status"]
-                                                  as NewsStatus)
-                                              .rawValue
-                                          : ""),
-                                      onPressed: () {},
-                                      style: ElevatedButton.styleFrom(
-                                        primary: (newsList[index]["status"]
-                                                is NewsStatus)
-                                            ? (newsList[index]["status"]
-                                                    as NewsStatus)
-                                                .primaryColor
-                                            : null, //Color.fromARGB(255, 198, 126, 210),
-                                        onPrimary: (newsList[index]["status"]
-                                                is NewsStatus)
-                                            ? (newsList[index]["status"]
-                                                    as NewsStatus)
-                                                .secondaryColor
-                                            : null, //Color.fromARGB(255, 108, 8, 125),
-                                      )),
-                                ),
-                              )
-                              // TextButton(
-                              //     child: Text("Add to cart".toUpperCase(),
-                              //         style: TextStyle(fontSize: 14)),
-                              //     style: ButtonStyle(
-                              //         padding:
-                              //             MaterialStateProperty.all<EdgeInsets>(
-                              //                 EdgeInsets.all(15)),
-                              //         foregroundColor:
-                              //             MaterialStateProperty.all<Color>(
-                              //                 Colors.red),
-                              //         shape: MaterialStateProperty.all<
-                              //                 RoundedRectangleBorder>(
-                              //             RoundedRectangleBorder(
-                              //                 borderRadius:
-                              //                     BorderRadius.circular(18.0),
-                              //                 side: BorderSide(
-                              //                     color: Colors.red)))),
-                              //     onPressed: () => null),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ))),
+            child: ListView.builder(
+          itemCount: newsList.length,
+          itemBuilder: (ctx, index) => NewsRecord(
+              title: ((newsList[index]["title"] is String)
+                  ? (newsList[index]["title"])
+                  : ""),
+              status: ((newsList[index]["status"] is NewsStatus)
+                  ? (newsList[index]["status"] as NewsStatus)
+                  : null),
+              category: (newsList[index]["category"] is NewsCategory)
+                  ? (newsList[index]["category"] as NewsCategory)
+                  : null),
 
           // child: ListView.builder(
           //   itemCount: 10,
@@ -222,7 +143,7 @@ class MainNewsScreen extends StatelessWidget {
           //         .toList()
           //   ],
           // ),
-        ));
+        )));
   }
 }
 
---
+++ lib/news_record.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+import 'news_category.dart';
+import 'news_status.dart';
+
+class NewsRecord extends StatelessWidget {
+  final NewsCategory category;
+  final String title;
+  final NewsStatus status;
+
+  NewsRecord(
+      {@required this.category, @required this.title, @required this.status});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(0),
+        height: 100,
+        // color: index % 2 == 0 ? Colors.amber : Colors.blue,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              primary: Colors.white,
+              onPrimary: Colors.black,
+              shadowColor: Colors.transparent,
+              padding: EdgeInsets.all(0)),
+          onPressed: () {},
+          child: Row(
+            children: [
+              Image.asset(category.imagepath ?? "assets/images/default.png"),
+              // Image.network(
+              //     "https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg"),
+              Container(
+                width: 10,
+              ),
+              Flexible(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                      softWrap: false,
+                      // style: TextStyle(backgroundColor: Colors.red),
+                    ),
+
+                    SizedBox(
+                      height: 25,
+                      child: IgnorePointer(
+                        ignoring: true,
+                        child: ElevatedButton(
+                            child: Text(status.rawValue ?? ""),
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              primary: status
+                                  .primaryColor, //Color.fromARGB(255, 198, 126, 210),
+                              onPrimary: status
+                                  .secondaryColor, //Color.fromARGB(255, 108, 8, 125),
+                            )),
+                      ),
+                    )
+                    // TextButton(
+                    //     child: Text("Add to cart".toUpperCase(),
+                    //         style: TextStyle(fontSize: 14)),
+                    //     style: ButtonStyle(
+                    //         padding:
+                    //             MaterialStateProperty.all<EdgeInsets>(
+                    //                 EdgeInsets.all(15)),
+                    //         foregroundColor:
+                    //             MaterialStateProperty.all<Color>(
+                    //                 Colors.red),
+                    //         shape: MaterialStateProperty.all<
+                    //                 RoundedRectangleBorder>(
+                    //             RoundedRectangleBorder(
+                    //                 borderRadius:
+                    //                     BorderRadius.circular(18.0),
+                    //                 side: BorderSide(
+                    //                     color: Colors.red)))),
+                    //     onPressed: () => null),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
