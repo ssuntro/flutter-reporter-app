2585b818ee4ea60829cd4770fbe0151a74345f31
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2022-06-05 17:27:57 +0700

    [AC-9] provider - add ChangeNotifierProvider.value at root of widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/main_news_page.dart';
+import 'package:news_app/news_list.dart';
 import 'package:news_app/news_page.dart';
 import 'package:news_app/splash_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,17 +13,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        MainNewsPage.routeName: (ctx) => MainNewsPage(),
-        // AddNewsPage.routeName: (ctx) => AddNewsPage(),
-        NewsPage.routeName: (ctx) => NewsPage(),
-      },
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SplashPage(title: 'News App'),
-    );
+    return ChangeNotifierProvider.value(
+        value: NewsList(),
+        child: MaterialApp(
+          routes: {
+            MainNewsPage.routeName: (ctx) => MainNewsPage(),
+            // AddNewsPage.routeName: (ctx) => AddNewsPage(),
+            NewsPage.routeName: (ctx) => NewsPage(),
+          },
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: SplashPage(title: 'News App'),
+        ));
   }
 }
