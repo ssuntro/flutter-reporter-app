0f0ede2fcfd11ad924dce4e23472c9d4a8df473c
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-08-12 17:47:42 +0700

    clean up, unncessary logic for integration testing

--- test/integration_testing.dart
+++ test/integration_testing.dart
@@ -8,50 +8,40 @@ void main() {
 
   testWidgets('Smoke test, Add news flow', (WidgetTester tester) async {
     app.main();
-
-    // await tester.tap(find.byIcon(Icons.add));
+    // Wit until Splash screen is gone
     await tester.pumpAndSettle(Duration(seconds: 6));
 
     // remember number of news before adding new news
-    // final listTileCountB4 = find.byType(ListTile).evaluate().length;
-    // print('Number of ListTile widgets: $listTileCountB4');
-    // Ensure the ReorderableListView is present
-    final reorderableListViewFinder = find.byType(ReorderableListView);
-    expect(reorderableListViewFinder, findsOneWidget);
-
-    // Get the number of children
-    final reorderableListView =
-        tester.widget<ReorderableListView>(reorderableListViewFinder);
-    final listTileCountB4 = reorderableListView.itemCount;
+    expect(find.byType(ReorderableListView), findsOneWidget);
+    final listTileCountB4 = tester
+        .widget<ReorderableListView>(find.byType(ReorderableListView))
+        .itemCount;
 
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump(Duration(seconds: 1));
 
+    //verify that we are on AddNewsPage
     expect(find.text('Add news'), findsOneWidget);
 
     // click "Add" button
     await tester.tap(find.text("Add"));
     await tester.pumpAndSettle();
 
-    //check number of item in ListTile +1
-    expect(find.byIcon(Icons.add), findsOneWidget);
-    await tester.pump();
-
-    final reorderableListViewFinderAfter = find.byType(ReorderableListView);
-    expect(reorderableListViewFinderAfter, findsOneWidget);
-    final reorderableListViewAfter =
-        tester.widget<ReorderableListView>(reorderableListViewFinderAfter);
-    final listTileCountfter = reorderableListViewAfter.itemCount;
-    expect(listTileCountfter, listTileCountB4 + 1);
+    //check number of item in increasing by 1
+    expect(find.byType(ReorderableListView), findsOneWidget);
+    final listTileCountAfter = tester
+        .widget<ReorderableListView>(find.byType(ReorderableListView))
+        .itemCount;
+    expect(listTileCountAfter, listTileCountB4 + 1);
   });
 
-  // testWidgets('smoke test, Read news', (WidgetTester tester) async {
-  //   app.main();
-  //   // await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pumpAndSettle(Duration(seconds: 6));
+  testWidgets('smoke test, Read news', (WidgetTester tester) async {
+    app.main();
+    //   // await tester.tap(find.byIcon(Icons.add));
+    //   await tester.pumpAndSettle(Duration(seconds: 6));
 
-  //   // Tap on the first news item
-  //   await tester.tap(find.byType(ListTile).first);
-  //   await tester.pumpAndSettle();
-  // });
+    //   // Tap on the first news item
+    //   await tester.tap(find.byType(ListTile).first);
+    //   await tester.pumpAndSettle();
+  });
 }
