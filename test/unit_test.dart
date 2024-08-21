44ede575deee42587c4148ff264ec032d6770aba
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-08-21 12:05:56 +0700

    add mockito in unit test suite

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,9 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
 import '../business_logic/calculator.dart';
+import 'package:mockito/mockito.dart';
+
+class MockCalculator extends Mock implements Calculator {}
 
 void main() {
-  test('addition test', () {
-    final calculator = Calculator();
-    expect(calculator.add(2, 3), 5);
+  // test('addition test', () {
+  //   final calculator = Calculator();
+  //   expect(calculator.add(2, 3), 5);
+  // });
+
+  test('fetchData returns data', () async {
+    final service = MockCalculator();
+    when(service.add(4, 5)).thenAnswer((_) => 111);
+
+    expect(await service.add(4, 5), 111);
   });
 }
