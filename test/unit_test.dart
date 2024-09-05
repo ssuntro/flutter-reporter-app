6634ec33fa83f1b9545bf89ffc13e317fcf5d66d
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-09-05 10:25:34 +0700

    spike, flutter_test without mockito

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,19 +1,32 @@
 import 'package:flutter_test/flutter_test.dart';
 import '../business_logic/calculator.dart';
-import 'package:mockito/mockito.dart';
 
-class MockCalculator extends Mock implements Calculator {}
+//NOTE: Spike: flutter_test without mockito, this file is for only spike purpose, . It is not a good practice to use this file in production code.
+
+class MockCalculator implements Calculator {
+  @override
+  int add(int a, int b) {
+    return 111;
+  }
+}
+
+// class MockCalculator extends Mock implements Calculator {
+//   @override
+//   int add(int a, int b) {
+//     return 111;
+//   }
+// }
 
 void main() {
-  // test('addition test', () {
-  //   final calculator = Calculator();
-  //   expect(calculator.add(2, 3), 5);
-  // });
+  test('addition test', () {
+    final calculator = Calculator();
+    expect(calculator.add(2, 3), 5);
+  });
 
   test('fetchData returns data', () async {
     final service = MockCalculator();
-    when(service.add(4, 5)).thenAnswer((_) => 111);
 
     expect(await service.add(4, 5), 111);
+    expect(await service.add(1, 1), 111);
   });
 }
